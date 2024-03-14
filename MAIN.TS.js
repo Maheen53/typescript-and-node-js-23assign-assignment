88a5ca986dd8bd763aaae49720e687e0ad65ef0a
Maheen Imtiaz <160345638+Maheen53@users.noreply.github.com>
2024-03-15 04:04:58 +0500

    Add files via upload

---
+++ MAIN.TS.js
@@ -0,0 +1,85 @@
+// Task 1
+// let personName : string  = "Eric";
+// console.log (`hello ${personName} , would you like to learn some python`);
+// task 2
+// let personName : string = "Maheen Imtiaz";
+// console.log(personName.toLowerCase());//show the persoName in small letters
+// console.log(personName.toUpperCase());//show the personName in big letters
+// console.log(personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase());//show the personName with the first letter 
+// task 3
+// let quote : string = "A person who never made a mistake never tried anything new.";
+// let authorName : string = "Albrert Einstine";
+// console.log(`once ${authorName} ,said ${quote}`)
+// task 5
+// let famousPerson : string = "Albert Einstine"
+// let message = "A person who never made a mistake never tried anything new."
+// console.log (`once ${famousPerson}, said ${message}`);
+//task 6
+// let persoName : string = "\t\n  Maheen Imtiaz \t\n";
+//console.log(persoName);// Show the name in whitespace
+// console.log(persoName.trim());// show the name in without whitespace
+// task 7 and 8
+//console.log(5+3)
+//console.log(10-2)
+//console.log(2*4)
+//console.log(16/2)
+//task 9
+// let favoriteNumber : string = "11"
+// console.log(`my  favorite number is ${favoriteNumber} `);
+//task 10
+// date :06 .03.24
+//my name is maheen imtiaz
+// let myName : string = "Maheen Imtiaz";
+// console.log(`hi ${myName} is here would you like to learn some python today`);
+// task 11
+// let  Name : string []= ["eda","shibra","alice"];
+// console.log(Name[0]);
+// console.log(Name[1]);
+//console.log(Name[2]);
+//task 12
+// let persoName : string [] = ["eda", "shibra" ,"alice"];
+// console.log(`hi ${persoName}  would you like to learn sometypescript today `);
+// task 13
+// let transport : string [] = ["hundai" , "suzuki" , "farari" , "honda"];
+// transport.forEach(transport => {
+//   console.log(`i would like to own a ${transport}.`)
+// });
+//task 14
+// let Guest : string [] = ["mari" , "mahbir", "radika" ,"alia"];
+// Guest.forEach(Guest => {
+//   console.log(`dear ${Guest} would you like to join us in a dinner`)
+// })
+//task 15
+// let Guest : string [] = ["mari" , "mahbir", "radika" ,"alia" ];
+//let newguest : string [] = ["hareem"]
+// console.log(`dear ${newguest} would you like to join us in a dinner?`);
+// replaceguest : string [] = ["mari"];
+// console.log(` ${replaceguest} unable to attend dinner `)
+// task 16
+//let Guest : string [] = ["hareem" , "mahbir", "radika" ,"alia" ];
+//console.log(`dear ${Guest} , i found a bigger dinning table`);
+//let newguest : string [] = ["hatat"]
+//.log(`dear ${newguest} you are invited adinner`);
+//let addingnewguest : string[] = ["sana"]
+//console.log(`dear ${addingnewguest} you are invited dinner`)
+//Guest.forEach(Guest=>{
+//   console.log(`dear ${Guest} would you like to join us in a dinner ?`)
+//});
+//console.log(Guest[0])
+////console.log(addingnewguest)
+//console.log(newguest) 
+//console.log(`dear  ${Guest[0]} ${addingnewguest} ${newguest} i invited to a dinner`)
+// task 17
+// let Guest : string [] = ["hareem" , "mahbir", "radika" ,"alia" ];
+// console.log(`dear ${Guest[0]}, ${Guest[1]} i can't reserve a bigger table so i invited only you two. `);
+//let replaceguest : string [] = ["radika","alia"];
+//console.log(`dear ${replaceguest} i am so so sorry i did not invited you in a dinner. `);
+//console.log(`dear ${replaceguest} you are still invited in a dinner`);
+//console.log(`dear ${replaceguest} you are still not invited so sorry`);
+//console.log(`dear ${Guest} you are sure to not invited because they are empty list. `);
+//task 18
+var places = ["dubai", "turkey", "maldieve", "london", "paris"];
+// Print the array in its original ordere 
+console.log("original order :", places);
+// print the array in its Alphabetical Order
+console.log("Alphabatical Order :", places);
---
+++ main.js
@@ -0,0 +1,37 @@
+// task 21
+// let book : {title:string; author:string; yearpublished: number;}=
+//{
+//   title :"a little life",
+//   author : "Hanya Yanagihara",
+//   yearpublished : 2015.
+//};
+//{console.log (`Book Info i like to read book in the moring today i read  ${book.title}  his author ${book.author} was so amazing ,${book.yearpublished} this amazing book published`)}
+// task 22
+//Intentional Eror
+// let friends : string [] = ["simran","raj","fara"];
+// console.log(friends[3]);
+// this is intentional error when typescript counting start with 0
+// console.log (friends[2]);
+// task 23
+ //conditional tests
+var car = "subaru";
+console.log("is car == `subaru`?i predict True.");
+console.log(car == "subaru");
+console.log("car == `subaru`? i predict false.");
+console.log(car == "honda");
+console.log("car == `subaru`? i predict false.");
+console.log(car == "civics");
+console.log("if 2 <= 4 ? i predict true.");
+console.log(2 <= 4);
+console.log("if  4 >= 2 ? i predict true");
+console.log(4 >= 2);
+console.log("if 45 >= 75 ? i predict false");
+console.log(45 >= 75);
+console.log("car == `hayat`? predict false");
+console.log(car == "hayat");
+console.log("is 3 <= 45 ? predict false");
+console.log(3 <= 2);
+console.log("is 72 >= 83 ? predict false");
+console.log(72 >= 83);
+console.log("is car ==`subaru`&& car.length ==6? predict true");
+console.log(car == "subaru" && car.length == 6);
